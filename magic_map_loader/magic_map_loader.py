0b4eff7d94062f11dce8616f8e74f7aa701b96ff
Skipper <skipperthepilot@googlemail.com>
2023-01-20 15:37:48 +0000

    Amending documentation
    Amending documentation, and removing a "Hello World" that slipped through....

--- magic_map_loader/magic_map_loader.py
+++ magic_map_loader/magic_map_loader.py
@@ -94,45 +94,7 @@ class MAGICMapLoader:
         status_tip=None,
         whats_this=None,
         parent=None):
-        """Add a toolbar icon to the toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
+                
         icon = QIcon(icon_path)
         action = QAction(icon, text, parent)
         action.triggered.connect(callback)
@@ -181,16 +143,20 @@ class MAGICMapLoader:
 
 
     def run(self):
-        print("Hello World")
+        
         # Get the bounding box from the map canvas
         canvas = self.iface.mapCanvas()
         extent = canvas.extent()
 
         # Check the CRS
         crs = canvas.mapSettings().destinationCrs()
+
+        # If the CRS is not British National Grid, throw an error message and do not proceed.
         if crs.authid() != 'EPSG:27700':
             self.iface.messageBar().pushMessage("Error", "The map canvas must be in OSGB 1936 / British National Grid", level=QgsMessageBar.CRITICAL)
             return
+        
+        #If it is in BNG - Proceed!
         else:
             # Get the coordinates of the bounding box
             xmin = extent.xMinimum()
